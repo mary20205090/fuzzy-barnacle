a9a88a01ae70f06d3549348bdee3cc5f0dd77e2d
mary20205090 <marymutua019@gmail.com>
2021-11-22 13:46:24 +0300

    changes updated

---
+++ footer.php
@@ -0,0 +1,5 @@
+<script src="asset/bootstrap-5.1.2-dist/js/jquery-3.6.0.min.js"></script>
+<script src="asset/bootstrap-5.1.2-dist/js/bootstrap.bundle.min.js"></script> 
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="asset/bootstrap-5.1.2-dist/css/bootstrap.min.css">  
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="asset/bootstrap-5.1.2-dist/css/style.css">
+     <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Estonia&family=Mochiy+Pop+One&display=swap" rel="stylesheet">
+</head>
+<body>
